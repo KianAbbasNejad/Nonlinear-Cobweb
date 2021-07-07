dafe1835cf867941e9f3f1cfadf9eae0e758f91e
Kian Abbas Nejad <kianabbasnejad@gmail.com>
2021-07-07 21:44:10 +0200

    Add files via upload
    Separated functions

--- Nonlinear_Cobweb.m
+++ Nonlinear_Cobweb.m
@@ -18,6 +18,7 @@
 %
 % $p^e_{t+1}=f(p_t^e)=(1-w)p_t^e+w \frac{a-d-\arctan[\lambda(p_t^e-c)]}{b}$
 %% Housekeeping
+
 clearvars
 close all
 clc
@@ -34,7 +35,7 @@ d = 0;
 Pmin = -10;
 Pmax = 10;
 step = 0.01;
-n = 1; %number of iterations
+n = 3; %number of iterations
 p0 = -6; %initial condition
 p1 = p0; %vector for iteration loop
 num = 5; % number of iterations for plotting
@@ -44,34 +45,8 @@ I = P; % vector for iterations
 % The Map
 fp = @(p) (1-w).*p + w.*(a-d-atan(lambda.*(p-c)))./b;
 
-%% Iteration Loop
-fig1= figure(1);
-hold on
-grid on
-fig1.Name = 'Iteration';
-ylabel('$p_{t+1}$','FontSize',16,'interpreter','latex')
-xlabel('$p_t$','FontSize',16,'interpreter','latex')
-title('Iteration of $p_{t+3}=f^3(p_t)$','FontSize',14,'interpreter','latex')
-axis([Pmin Pmax Pmin Pmax])
-plot(P,P,'b--')
-
-for i=1:n
-    I=fp(I);
-end
-
-% Plotting the nth iteration of the map
-plot(P,I,'r')
-
-for i=1:num
-    for k=1:n
-        p1 =fp(p1);
-    end
-    plot([p0 p0],[p0 p1],'k')
-    plot([p0 p1],[p1 p1],'k')
-    p0 = p1;
-end
-
 %% Time Series
+
 tmax = 50;
 trans = 0; %number of transient iterations not to plot
 T = 1:tmax;
@@ -92,110 +67,18 @@ ylabel('$p_t$','FontSize',16,'interpreter','latex');
 xlabel('$t$','FontSize',16,'interpreter','latex');
 title('Time Series of $p_t$','FontSize',14,'interpreter','latex');
 
+%% Iteration Loop
+
+IterationMapFun(fp,n,p0,num,'p',[-10 10 -10 10]) 
+
 %% Bifurcation Diagram
-%{
-pmin = 0.2; %parameter minimum
-pmax = 0.9; %parameter max
-pstep = 0.001; %parameter step size
-tmax = 1000; % number of total iterations
-trans = 500; %number of initial points not to plot
-P = zeros(tmax,1);
-
-w = pmin:pstep:pmax;
-
-%Redefine the map in terms of the parameter: 
 
 fp = @(p,W) (1-W).*p + W.*(a-d-atan(lambda.*(p-c)))./b;
 
-% initialise bifurcation diagram
-fig3=figure(3);
-fig2.Name = 'Bifurcation';
-title('Bifurcation Diagram','FontSize',14,'interpreter','latex');
-ylabel('$p_t$','FontSize',14,'interpreter','latex');
-xlabel('$w$','FontSize',14,'interpreter','latex');
+BifurcationFun(fp,0.2:0.001:0.9,500,1000,'p','w')
 
-hold on
-for k=1:length(w) % first loop iterates through parameter
-    for i=2:tmax %same as before
-        pp = P(i-1);
-        P(i)=fp(pp,w(k));
-    end
-    % create a vector of w(k) in order to plot it:
-    B = ones(1,tmax-trans)*w(k);
-    scatter(B,P(trans+1:tmax),0.03,'k');
-end
-%}
+%% Basin of Attraction
 
-%% Color Map for Parameter Basins
-% Defining a colormap for up to 10 cycles
-% non-convergent(-1) = white    divergent(0) = black
-% steady-state  = red           2-cycle = blue
-% 3-cycle = yellow              4-cycle = orange
-% 5-cycle = gray                6-cycle = teal
-% 7-cycle = purple              8-cycle = olive
-% 9-cycle = green               10-cycle = brown
-cmap = [1 1 1; 0 0 0
-    1 0 0; 0 0 1
-    1 1 0; 1 0.5 0
-    0.5 0.5 0.5; 0 0.5 0.5
-    0.5 0 0.5 ; 0.5 0.5 0
-    0 0.5 0; 0.5 0.27 0.1];
-% labels for legend:
-labels = {'Non-convergent','divergent','steady-state','2-cycle','4-cycle',...
-    '5-cycle','6-cycle','7-cycle','8-cycle','9-cycle','10-cycle'};
-%% Parameter Basins for Periodic Cycles 
-
-cycle_num = 7; %number of periodic cycles 
-contour_levels= -1:cycle_num; 
-max_dist = 1000;  % maximum distance after which the point is classified as 'divergent'
-tol = 3; %rounding tolerance '1e-tol' for number of periodic cycles
-p0 = 6; %initial condition
-tmax = 1000; %number of iterations
-trans = 700; %number of transient iterations
-
-Px_min = 0; %minimum value for x-axis parameter (here lambda)
-Px_max = 10;
-Py_min = 0.1;
-Py_max = 0.8;
-step = 0.002;
-
-lambda = Px_min:step:Px_max; %lambda is x-axis param.
-w = Py_min:step:Py_max;
-
-%Redefine the function in terms of the parameter: 
 Fp = @(p,L,W) (1-W).*p + W.*(a-d-atan(L.*(p-c)))./b;
 
-[PX, PY]=meshgrid(lambda, w);
-Basin = -ones(numel(w),numel(lambda)); %Parameter basin matrix. -1 is non convergent
-
-for i=1:numel(PX)
-    P = [p0; zeros(tmax,1)];
-    for j=2:tmax %same as before
-        pp = P(j-1);
-        P(j)=Fp(pp,PX(i),PY(i));
-    end  
-    P=round(P,tol); %rounding using the tolerance level
-    C = numel(unique(P(trans:tmax))); 
-    if (P(end) - P(1)) > max_dist % if divergent
-        Basin(i) = 0; %0 is color code for divergent
-    elseif C <= cycle_num
-        Basin(i)= C;
-    end 
-end
-fig4= figure(4);
-hold on
-[M,c]=contourf(lambda,w,Basin,contour_levels);
-cmap = cmap(1:cycle_num+2,:);
-colormap(cmap)
-fig2.Name = 'Basin_of_Attraction';
-title('Parameter Basin of Attraction','FontSize',14,'interpreter','latex');
-ylabel('$w$','FontSize',14,'interpreter','latex');
-xlabel('$\lambda$','FontSize',14,'interpreter','latex');
-
-% Creating Legend
-h = zeros(cycle_num+2,1);
-for i=1:cycle_num+2
-    h(i) = plot(NaN,NaN,'o','Color',cmap(i,:));
-end
-lgd = legend(h, labels(1:cycle_num+2),'Location','southoutside','interpreter','latex');
-lgd.NumColumns = 4;
+BasinFun(Fp,7,0:0.002:10,0.1:0.002:0.8,6,300,400,'\lambda','w')
